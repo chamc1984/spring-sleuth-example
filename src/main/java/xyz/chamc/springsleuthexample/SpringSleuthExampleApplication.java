447501b09ba1dbee0f1075ade223b1f0a6d62fe6
chamc1984 <chamc1984@gmail.com>
2019-01-07 00:05:32 +0900

    初回commit

---
+++ src/main/java/xyz/chamc/springsleuthexample/SpringSleuthExampleApplication.java
@@ -0,0 +1,14 @@
+package xyz.chamc.springsleuthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSleuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSleuthExampleApplication.class, args);
+	}
+
+}
+
